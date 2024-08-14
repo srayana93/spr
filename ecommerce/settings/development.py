4efe5d6f7e4a61e8eba9a7d03a0a5e7174be37a8
srayana93 <34994055+srayana93@users.noreply.github.com>
2024-08-14 08:56:27 +0000

    Initial commit

---
+++ ecommerce/settings/development.py
@@ -0,0 +1,4 @@
+from .base import *
+
+# Development settings here
+DEBUG = True
---
+++ ecommerce/settings/production.py
@@ -0,0 +1,5 @@
+from .base import *
+
+# Production settings here
+DEBUG = False
+ALLOWED_HOSTS = ['your-production-domain.com']
